f7730f4178fd039f24bc850aaeb0c4d145e27394
Derrik Diener <142091676+soltros@users.noreply.github.com>
2026-06-20 15:16:24 -0400

    fix: express 5 wildcard routing crash

--- server/index.js
+++ server/index.js
@@ -197,7 +197,7 @@ app.post('/api/metadata/refresh', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '..', 'dist')));
   
-  app.get('*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
   });
 }

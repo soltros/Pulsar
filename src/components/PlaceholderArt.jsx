65bdeac8f3a9c4ee01fba98ce7d13f75115280f5
Derrik Diener <142091676+soltros@users.noreply.github.com>
2026-06-21 03:13:52 -0400

    style: change PulsarLogo to use primary color in PlaceholderArt

--- src/components/PlaceholderArt.jsx
+++ src/components/PlaceholderArt.jsx
@@ -4,7 +4,7 @@ export default function PlaceholderArt({ className = "w-full h-full", iconClassN
   return (
     <div className={`p-[1px] bg-gradient-to-br from-primary to-orange-400 ${className} rounded-[inherit]`}>
       <div className="w-full h-full bg-black rounded-[inherit] flex items-center justify-center">
-        <PulsarLogo className={`${iconClassName} text-white`} />
+        <PulsarLogo className={`${iconClassName} text-primary`} />
       </div>
     </div>
   );
